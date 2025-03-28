98b5474159a01513a2d36334d2c8a8977f0b36d0
maroussia <soamaroussia@gmail.com>
2025-03-28 11:59:51 +0300

    update .env

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ app.use("/api/vehicles", vehicleRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/billing", billingRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
